2bb95f2e627fd8d706a17d7ac660142c0d79c2b9
Satyam <satyamagrawalread@gmail.com>
2024-08-20 22:20:27 +0530

    Cors policy modify

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ module.exports = {
     var io = new Server(strapi.server.httpServer, {
       cors: {
         // cors setup
-        origin: "http://localhost:5173",
+        origin: ['*'],
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
         credentials: true,
